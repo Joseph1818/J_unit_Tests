8a3388faec5bb1b4c7d6a347ae93305420f00f96
Umba joseph <Josephdjamba04@gmail.com>
2024-09-19 13:14:30 +0200

    add/devide/radius methods added

--- src/main/java/org/example/MathUtils.java
+++ src/main/java/org/example/MathUtils.java
@@ -1,7 +1,20 @@
 package org.example;
 
 public class MathUtils {
+
   public int add (int a , int  b ) {
       return a + b ;
   }
+ public double computerCircleRadius(double radius){
+      return Math.PI * radius * radius;
+  }
+  public int divide (int a, int b) {
+      return a  / b;
+  }
+    public int multiply (int a, int b) {
+        return a  * b ;
+    }
+    public int subtract (int a, int b) {
+        return a  - b;
+    }
 }
--- src/test/java/org/example/MathUtilsTest.java
+++ src/test/java/org/example/MathUtilsTest.java
@@ -1,16 +1,38 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+
     /**
      * Method under test: {@link MathUtils#add(int, int)}
      */
+
     @Test
     void testAdd() {
-        System.out.println("This is the test ran");
+     //Creating a new instance
+        MathUtils underTest = new MathUtils();
+        //Expected output;
+        int expected = 2;
+        // Actual output;
+        int actual = underTest.add(1,1);
+        assertEquals(expected, actual, "The add methode should have two numbers");
     }
+
+    @Test
+    void testRadiusCircle(){
+        MathUtils underTest = new MathUtils();
+        assertEquals(314.1592653589793, underTest.computerCircleRadius(10),"if failed please review the math");
+    }
+
+    @Test
+    void testDivide(){
+        MathUtils mathUtils = new MathUtils();
+        assertThrows( ArithmeticException.class,() -> mathUtils.divide(1,0), "Divide test by Zero should trow ");
+    }
+
 }
 
